f95c9f19aa72757051ba03dc5535f8d3ab159879
Islambek-Urazbaev <128123292+Islambek-Urazbaev@users.noreply.github.com>
2023-07-21 11:34:21 +0500

    Initial

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// let number = 15;
+// const borderColor = "red";
+// console.log(number);
+
+// let number;
+// console.log(number);
+
+// console.log(num);
+
+// const place = confirm("Are you ready?");
+// console.log(place);
+
+// const currentPlace = prompt('Do you speak english', 'Islambek')
+
+// const Color = [];
+
+// Color[0] = prompt("What is your color #1", "");
+// Color[1] = prompt("What is your color #2", "");
+// Color[2] = prompt("What is your color #3", "");
+
+// console.log(Color);
+
+// let channel = prompt("What's is fov youtuber?");
+// console.log(`https://youtube.com/${channel}`);
